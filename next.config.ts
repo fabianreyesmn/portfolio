b2401b30c9b52733e1ce8fd6583f5e46dae2818b
Fabián Reyes <reyesfm07@gmail.com>
2025-06-27 01:16:17 +0000

    puedo subirlo a github pages?

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type {NextConfig} from 'next';
 
+// IMPORTANT: Replace 'TechFolio' with your GitHub repository name.
+const repo = 'TechFolio';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +15,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
